c403fdc6d706dbc6f93ca35f594dcd989a7730f2
b4shtirk1n <wowan4512@gmail.com>
2024-02-27 18:37:46 +0500

    create web

---
+++ dockercraft-web/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "../styles/Header.scss";
+
+export default function Header() {
+  return (
+    <header>
+      <a href="#"></a>
+      <div>
+        <a href="#">Docs</a>
+        <a href="#">Docker hub</a>
+        <a href="#">Github</a>
+      </div>
+    </header>
+  );
+}
---
+++ dockercraft-web/src/pages/App.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import "./App.css";
+
+export default function App() {
+  return (
+    <section>
+      <Header/>
+      <h1>Dockercraft</h1>
+      <p>Docker image for minecraft </p>
+    </section>
+  );
+}
