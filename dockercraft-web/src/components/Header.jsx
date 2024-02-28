44e89b2b37544d5005205e4e599d33399b00cbee
b4shtirk1n <wowan4512@gmail.com>
2024-02-28 05:59:11 +0500

    edit home page

--- dockercraft-web/src/components/Header.jsx
+++ dockercraft-web/src/components/Header.jsx
@@ -4,13 +4,13 @@ import "../styles/Header.scss";
 export default function Header() {
   return (
     <header>
-      <a href="#">
+      <a href="/">
         <img className="home-btn" src={logo} />
       </a>
       <div>
         <a href="#">Docs</a>
         <a href="#">Docker hub</a>
-        <a href="#">Github</a>
+        <a href="https://github.com/b4shtirk1n/Dockercraft">Github</a>
       </div>
     </header>
   );
--- dockercraft-web/src/pages/App.jsx
+++ dockercraft-web/src/pages/App.jsx
@@ -1,4 +1,5 @@
-import logo from "../assets/logo.svg"
+import logo from "../assets/logo.svg";
+import copy from "../assets/copy.svg";
 import Header from "../components/Header";
 import "../styles/App.scss";
 
@@ -10,8 +11,16 @@ export default function App() {
         <div className="intro-container">
           <h1 className="title">Dockercraft</h1>
           <p className="description">Docker image for minecraft </p>
+          <pre>
+            <code className="bash">
+              docker run -d -p 25565:25565 b4shtirk1n/minecraft-server
+            </code>
+            <a className="copy-btn">
+              <img src={copy}></img>
+            </a>
+          </pre>
         </div>
-        <img className="logo" src={logo}/>
+        <img className="logo" src={logo} />
       </section>
     </>
   );
