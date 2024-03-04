6687ca5011153be6bb0f00a197da097b41e16d49
b4shtirk1n <wowan4512@gmail.com>
2024-03-04 11:48:43 +0500

    url graber

--- main.py
+++ main.py
@@ -12,28 +12,29 @@ FORGE_VER: str = os.getenv("FORGE_VER")
 NEOFORGE_VER: str = os.getenv("NEOFORGE_VER")
 FABRIC_LOADER_VER: str = os.getenv("FABRIC_LOADER_VER")
 FABRIC_VER: str = os.getenv("FABRIC_VER")
+BEDROCK_VER: int = os.getenv("BEDROCK_VER")
 
 types: list[Type] = [
     Type("VANILLA", f"https://mcversions.net/download/{MC_VER}"),
-    Type("FORGE", f"https://maven.minecraftforge.net/net/minecraftforge/forge/$MC_VER-$FORGE_VER/forge-{MC_VER}-{FORGE_VER}-installer.jar"),
-    Type("NEO_FORGE", f"https://maven.neoforged.net/releases/net/neoforged/neoforge/{NEOFORGE_VER}/neoforge-{NEOFORGE_VER}-installer.jar"),
-    Type("FABRIC", f"https://meta.fabricmc.net/v2/versions/loader/{MC_VER}/{FABRIC_LOADER_VER}/{FABRIC_VER}/server/jar")
+    Type("FORGE", "https://maven.minecraftforge.net/net/minecraftforge/forge/"
+         + f"{MC_VER}-{FORGE_VER}/forge-{MC_VER}-{FORGE_VER}-installer.jar"),
+    Type("NEO_FORGE", "https://maven.neoforged.net/releases/net/neoforged/neoforge/"
+         + f"{NEOFORGE_VER}/neoforge-{NEOFORGE_VER}-installer.jar"),
+    Type("FABRIC", "https://meta.fabricmc.net/v2/versions/loader/"
+         f"{MC_VER}/{FABRIC_LOADER_VER}/{FABRIC_VER}/server/jar"),
+    Type("BEDROCK", f"https://minecraft.azureedge.net/bin-linux/bedrock-server-{BEDROCK_VER}.zip")
 ]
 
 def get_url() -> str:
     url: str = "NULL"
-    if (TYPE != None):
+    if TYPE != None:
         url = types[__name__ == TYPE].url
-        if (TYPE == "FORGE"):
-            return parse(url)
+        if TYPE == "FORGE":
+            return parse(url, "https://piston-data.mojang.com", "jar")
         return url
     return url
 
-def parse(
-    url: str,
-    find: str = "https://piston-data.mojang.com",
-    find_last: str = "jar"
-) -> str:
+def parse(url: str, find: str, find_last: str) -> str:
     r: str = requests.get(url).text
     index: int = r.find(find)
     last: int = r.find(find_last)+len(find_last)
